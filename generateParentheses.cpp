e89759400da61b08f4e5620017e1fe6e006f8811
Kevin Cheng <ksxcheng@ucdavis.edu>
2017-01-05 00:51:07 -0800

    add files

--- generateParentheses.cpp
+++ generateParentheses.cpp
@@ -1,40 +1,31 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-void dfs(int num, int leftP, int rightP, string temp, string& paran)
+class Solution {
+public:
+void dfs(int num, int leftP, int rightP, string temp, int depth, vector<string>& answer)
 {
   string copy = temp;
-  if (rightP == num)
+  if (depth == 2 * num)
   {
-    cout << "***" << endl;
-    cout << temp << endl;
-    cout << "leftP: " << leftP << endl;
-    cout << "rightP: " << rightP << endl;
-    cout << "****" << endl;
+    answer.push_back(temp);
     return;
   }
-    if (leftP < num)
-    {
-      temp+= "(";
-      dfs(num, leftP + 1, rightP, temp, paran);
-    }
-    if (rightP < num)
-    {
-      temp+= ")";
-      dfs(num, leftP, rightP + 1, temp, paran);
-    }
-}
-void generate(int num)
-{
-  string paran = "()";
-  dfs(num, 1, 0, "(", paran);
+  if (leftP < num)
+  {
+    temp+=  '(';
+    dfs(num, leftP + 1, rightP, temp, depth + 1, answer);
+  }
+  temp = copy;
+  if (rightP < num && rightP < leftP)
+  {
+    temp+= ')';
+    dfs(num, leftP, rightP + 1, temp, depth + 1, answer);
+  }
 }
 
-int main()
-{
-  int num;
-  cout << "Enter total pairs: " << endl;
-  cin >> num;
-  generate(num);
-}
+    vector<string> generateParenthesis(int n) {
+        string temp;
+        temp += "(";
+        vector<string> answer;
+        dfs(n, 1, 0, temp, 1, answer);
+        return answer;
+    }
+};
